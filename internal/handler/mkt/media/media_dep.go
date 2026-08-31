821cfd499023535d69a6a06e68f24da739e45cd9
nicholas <nicholas@youlun88.com>
2026-08-31 20:54:01 +0800

    头条广告数据新增优化目标

--- internal/handler/mkt/media/media_dep.go
+++ internal/handler/mkt/media/media_dep.go
@@ -15,7 +15,9 @@ type MediaDepHandler struct {
 	service *mediaSvc.MediaDepService
 }
 
-func NewMediaDepHandler() *MediaDepHandler { return &MediaDepHandler{service: mediaSvc.NewMediaDepService()} }
+func NewMediaDepHandler() *MediaDepHandler {
+	return &MediaDepHandler{service: mediaSvc.NewMediaDepService()}
+}
 
 func (h *MediaDepHandler) Create(c *gin.Context) {
 	var m media.MediaDep
@@ -35,16 +37,32 @@ func (h *MediaDepHandler) Create(c *gin.Context) {
 }
 
 func (h *MediaDepHandler) GetList(c *gin.Context) {
-	page, _ := strconv.Atoi(c.DefaultPostForm("page", "1"))
-	size, _ := strconv.Atoi(c.DefaultPostForm("size", "10"))
-	keyword := c.DefaultPostForm("keyword", "")
-	status, _ := strconv.Atoi(c.DefaultPostForm("status", "-1"))
-	list, total, err := h.service.FindPage(page, size, keyword, status)
+	var req struct {
+		Page    int    `json:"page"`
+		Size    int    `json:"size"`
+		Keyword string `json:"keyword"`
+		Status  *int   `json:"status"`
+	}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		response.Error(c, http.StatusBadRequest, "参数错误: "+err.Error())
+		return
+	}
+	if req.Page < 1 {
+		req.Page = 1
+	}
+	if req.Size < 1 {
+		req.Size = 10
+	}
+	status := -1
+	if req.Status != nil {
+		status = *req.Status
+	}
+	list, total, err := h.service.FindPage(req.Page, req.Size, req.Keyword, status)
 	if err != nil {
 		response.Error(c, http.StatusInternalServerError, err.Error())
 		return
 	}
-	response.PageSuccess(c, list, total, page, size)
+	response.PageSuccess(c, list, total, req.Page, req.Size)
 }
 
 func (h *MediaDepHandler) GetAll(c *gin.Context) {
